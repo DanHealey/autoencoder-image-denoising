b7f01f5fe203c2206cd0f8a1abe0fcb30d3eface
pmcurtin <peter_curtin@brown.edu>
2023-04-22 13:02:55 -0400

    new model

--- code/autoencoder.py
+++ code/autoencoder.py
@@ -2,7 +2,16 @@ import tensorflow as tf
 
 from tensorflow.keras.models import Model
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Conv2DTranspose
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Conv2DTranspose, LeakyReLU
+
+# Noise parameters
+NOISE_MEAN = 0
+NOISE_STD = 30/255
+
+def noiser(original_image):
+    noise = tf.random.normal(tf.shape(original_image), mean=NOISE_MEAN, stddev=NOISE_STD)
+    noisy_image = tf.clip_by_value(original_image + noise, 0, 1)
+    return noisy_image
 
 class Autoencoder(Model):
     def __init__(self, latent_dim=512, input_size=(150, 150)):
@@ -14,16 +23,137 @@ class Autoencoder(Model):
         self.input_size = input_size
         
         # Encoder
-        self.encoder = Sequential([
-            Conv2D(32, (3, 3), activation="relu", padding="same"),
-            MaxPooling2D((2, 2), padding="same")
-        ])
+        #self.encoder = Sequential([
+            
+        #])
+
+        self.c1_1 = Conv2D(3, (3, 3), padding='SAME')
+        self.c1_2 = LeakyReLU()
+        self.c1_3 = Conv2D(16, (3, 3), padding='SAME')
+        self.c1_4 = LeakyReLU()
+        self.c1_5 = MaxPooling2D(padding='SAME')
+        self.bn_1 = tf.keras.layers.BatchNormalization()
+
+        #Conv2D(16, (3, 3), padding='SAME'),
+        #LeakyReLU(),
+        self.c2_1 = Conv2D(64, (3, 3), padding='SAME')
+        self.c2_2 = LeakyReLU()
+        self.bn_2 = tf.keras.layers.BatchNormalization()
+        #MaxPooling2D(padding='SAME'),
+
+        self.c3_1 = Conv2D(64, (3, 3), padding='SAME')
+        self.c3_2 = LeakyReLU()
+        self.c3_3 = Conv2D(256, (3, 3), padding='SAME')
+        self.c3_4 = LeakyReLU()
+        self.bn_3 = tf.keras.layers.BatchNormalization()
+        #MaxPooling2D(padding='SAME'),
+
+        self.c3_5 = Conv2D(256, (3, 3), padding='SAME')
+        self.c3_6 = LeakyReLU()
+        #Conv2D(256, (3, 3), padding='SAME'),
+        #LeakyReLU(),
 
         # Decoder
-        self.decoder = Sequential([
-            Conv2DTranspose(1, (3, 3), strides=2, activation="sigmoid", padding="same")
-        ])
+        # self.decoder = Sequential([
+            
+        # ])
+
+        self.c4_01 = Conv2DTranspose(256, (3, 3), padding='SAME')
+        self.c4_02 = LeakyReLU()
+        #Conv2DTranspose(256, (3, 3), padding='SAME'),
+
+        #Conv2DTranspose(256, (3, 3), strides=2, padding='SAME'),
+        #LeakyReLU(),
+        self.c4_1 = Conv2DTranspose(256, (3, 3), padding='SAME')
+        self.c4_2 = LeakyReLU()
+        self.c4_3 = Conv2DTranspose(64, (3, 3), padding='SAME')
+        self.c4_4 = LeakyReLU()
+        self.bn_1 = tf.keras.layers.BatchNormalization()
+        self.bn_4 = tf.keras.layers.BatchNormalization()
+
+        #Conv2DTranspose(64, (3, 3), strides=2, padding='SAME'),
+        
+        self.c5_1 = Conv2DTranspose(64, (3, 3), padding='SAME')
+        self.c5_2 = LeakyReLU()
+        self.bn_5 = tf.keras.layers.BatchNormalization()
+        #Conv2DTranspose(16, (3, 3), padding='SAME'),
+
+        self.c6_1 = Conv2DTranspose(16, (3, 3), strides=2, padding='SAME')
+        self.c6_2 = LeakyReLU()
+        self.bn_6 = tf.keras.layers.BatchNormalization()
+        #Conv2DTranspose(16, (3, 3), padding='SAME'),
+        #LeakyReLU(),
+        self.c6_3 = Conv2DTranspose(3, (3, 3), padding='SAME', activation='sigmoid')
         
     def call(self, x):
-        return self.decoder(self.encoder(x))
-    
+
+        x = self.c1_1(x)
+        x = self.c1_2(x)
+        x = self.c1_3(x)
+        x = self.c1_4(x)
+        x = self.c1_5(x)
+        x = self.bn_1(x)
+
+        c2_1 = self.c2_1(x)
+        x = self.c2_2(c2_1)
+        x = self.bn_2(x)
+
+        c3_1 = self.c3_1(x)
+        x = self.c3_2(c3_1)
+        x = self.c3_3(x)
+        x = self.c3_4(x)
+        x = self.bn_3(x)
+
+        x = self.c3_5(x)
+        x = self.c3_6(x)
+
+        x = self.c4_01(x)
+        x = self.c4_02(x)
+
+        x = self.c4_1(x)
+        x = self.c4_2(x)
+        x = self.c4_3(x)
+        x = self.c4_4(x) + c3_1
+        x = self.bn_4(x)
+
+        x = self.c5_1(x) + c2_1
+        x = self.c5_2(x)
+        x = self.bn_5(x)
+
+        x = self.c6_1(x)
+        x = self.c6_2(x)
+        x = self.bn_6(x)
+        x = self.c6_3(x) 
+
+
+        return x
+
+    def train_step(self, inputs):
+
+        inputs, labels = inputs
+
+        noisy = noiser(inputs)
+
+        with tf.GradientTape() as tape:
+
+            denoised = self(noisy)
+            loss = self.compiled_loss(inputs, denoised)
+
+        grads = tape.gradient(loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
+
+        self.compiled_metrics.update_state(inputs, denoised)
+
+        return {m.name: m.result() for m in self.metrics}
+
+    def test_step(self, inputs):
+
+        inputs, labels = inputs
+
+        noisy = noiser(inputs)
+
+        denoised = self(noisy)
+
+        self.compiled_metrics.update_state(inputs, denoised)
+
+        return {m.name: m.result() for m in self.metrics}
--- code/run.py
+++ code/run.py
@@ -7,39 +7,44 @@ from autoencoder import Autoencoder
 from tensorflow.keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator
 
 # Image data 
-TRAIN_DATA_DIRECTORY = "../data/data/train"
-TEST_DATA_DIRECTORY = "../data/data/test"
-IMAGE_SIZE = (150, 150)
+TRAIN_DATA_DIRECTORY = "C:/datasets/bim"
+TEST_DATA_DIRECTORY = "C:/datasets/bim"
+IMAGE_SIZE = (256, 256)
 
 # Visualization data
-VISUALIZE_IMAGE_PATH = "../data/data/validation/130.jpg"
+VISUALIZE_IMAGE_PATH = "C:/datasets/intel/seg_pred/5.jpg"
 
 # Noise parameters
 NOISE_MEAN = 0
-NOISE_STD = 30
+NOISE_STD = 30/255
 
 # Model hyperparameters
-EPOCHS = 1
-BATCH_SIZE = 32
-LEARNING_RATE = 0.01
+EPOCHS = 3
+BATCH_SIZE = 8
+LEARNING_RATE = 0.0005
 VALIDATION_SPLIT = 0.2
 LATENT_DIM=512
 
 def noiser(original_image):
-    noise = tf.random.normal(original_image.shape, mean=NOISE_MEAN, stddev=NOISE_STD)
+    noise = tf.random.normal(tf.shape(original_image), mean=NOISE_MEAN, stddev=NOISE_STD)
     noisy_image = tf.clip_by_value(original_image + noise, 0, 1)
     return noisy_image
 
+
+def resize(images):
+    return tf.image.resize(images, (152, 152))
+
 def visualize_results(model):
-    image = load_img(VISUALIZE_IMAGE_PATH, target_size=IMAGE_SIZE)
+    image_path = "C:/datasets/intel/seg_pred/" + input("filename")
+    image = load_img(image_path, target_size=IMAGE_SIZE)
     image = img_to_array(image)
-    image_input = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-    noisy_image = noiser(image)
+    image_input = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))/255
+    noisy_image = noiser(image_input)
     model_output = model(noisy_image)
 
-    Image.fromarray(np.asarray(image)).show()
-    Image.fromarray(np.asarray(noisy_image)).show()
-    Image.fromarray(np.asarray(model_output)).show()
+    Image.fromarray((np.asarray(image_input).squeeze()*255).astype(np.uint8)).show()
+    Image.fromarray((np.asarray(noisy_image).squeeze()*255).astype(np.uint8)).show()
+    Image.fromarray((np.asarray(model_output).squeeze()*255).astype(np.uint8)).show()
 
 def main():
     autoencoder = Autoencoder(LATENT_DIM, IMAGE_SIZE)
@@ -51,7 +56,8 @@ def main():
         zoom_range=0,
         horizontal_flip=True,
         vertical_flip=True,  
-        preprocessing_function=noiser     
+        validation_split=VALIDATION_SPLIT
+        #preprocessing_function=resize     
     )
 
     train_generator = train_datagen.flow_from_directory(
@@ -61,26 +67,39 @@ def main():
         classes=None,
         class_mode="input",
         shuffle=True,
+        subset='training',
+        keep_aspect_ratio=True,
     )
 
 
     test_datagen = ImageDataGenerator(
-        rescale=1.0/255.0    
+        rescale=1.0/255.0,
+        #preprocessing_function=resize
     )
 
-    test_generator = test_datagen.flow_from_directory(
+    test_generator = train_datagen.flow_from_directory(
         TEST_DATA_DIRECTORY,
         batch_size=BATCH_SIZE,
         target_size=IMAGE_SIZE,
         classes=None,
         class_mode="input",
-        shuffle=True
+        shuffle=True,
+        subset='validation',
+        keep_aspect_ratio=True,
     )
     
-    optimizer = tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE)
-    loss = tf.keras.losses.MeanSquaredError()
+    optimizer = tf.optimizers.Adam(learning_rate=LEARNING_RATE)
+    def psnr_loss(y_true, y_pred):
+        return -tf.image.psnr(y_true, y_pred, max_val=1.0)
+    def ssim_loss(y_true, y_pred):
+        return -tf.image.ssim(y_true, y_pred, 1)
+    mse = tf.keras.losses.MeanSquaredError()
+    def mix(y_true, y_pred):
+        return 0.5 * ssim_loss(y_true, y_pred) + 0.5 * mse(y_true, y_pred)
+    loss = ssim_loss
     metrics = [
-        tf.keras.metrics.MeanSquaredError()
+        tf.keras.metrics.MeanSquaredError(),
+        psnr_loss,
     ]
 
     autoencoder.compile(
@@ -89,6 +108,9 @@ def main():
         metrics=metrics
     )
 
+    autoencoder.build(input_shape=(1, IMAGE_SIZE[0], IMAGE_SIZE[1], 3))
+    autoencoder.summary()
+
     autoencoder.fit(
         train_generator,
         epochs=EPOCHS,
@@ -96,7 +118,10 @@ def main():
         validation_data=test_generator
     )
 
-    visualize_results(autoencoder)
+    autoencoder.save_weights('weights_ssim_only')
+
+    for i in range(10):
+        visualize_results(autoencoder)
 
 if __name__ == "__main__":
     main()
